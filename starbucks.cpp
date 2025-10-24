d6ac209fa385cbf96389e14f9b9b3777e24f0d83
Shivam Srivastava <srivastavashivam659@gmail.com>
2025-10-25 03:52:08 +0530

    Update starbucks.cpp

--- starbucks.cpp
+++ starbucks.cpp
@@ -1,12 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-int main() {
-    int money;
-    cin >> money;
-    if(money = 1000)
-    cout << "let go home" <<endl;
-    cout << "coffee in starbucks";
-
-
-}
