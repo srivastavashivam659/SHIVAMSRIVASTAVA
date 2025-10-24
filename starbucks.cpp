1a6e2c326813ba9cd8f441dbb4046d466e25e14d
Shivam Srivastava <srivastavashivam659@gmail.com>
2025-10-25 03:38:06 +0530

    my second file

---
+++ starbucks.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int money;
+    cin >> money;
+    if(money = 1000)
+    cout << "let go home" <<endl;
+    cout << "coffee in starbucks";
+
+
+}
